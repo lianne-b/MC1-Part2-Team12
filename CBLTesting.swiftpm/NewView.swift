b98bab82cc0fde60f9cc37c7da8aa99495cad301
Toughie <airj907@gmail.com>
2023-03-29 22:08:11 +0900

    NavigationBarBackButtonHidden

---
+++ CBLTesting.swiftpm/NewView.swift
@@ -0,0 +1,20 @@
+//
+//  NewView.swift
+//  CBLTesting
+//
+//  Created by KimChoonSik on 2023/03/29.
+//
+
+import SwiftUI
+
+struct NewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewView()
+    }
+}
