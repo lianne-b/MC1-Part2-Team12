bfcc6b34707d917bc8d4354d3fd7995e0cf13a0d
BruniDev <hyeondxx@gmail.com>
2023-03-30 16:12:48 +0900

    Add MenuView StartView

---
+++ CBLTesting.swiftpm/MenuView.swift
@@ -0,0 +1,14 @@
+//
+//  MenuView.swift
+//  CBLTesting
+//
+//  Created by 정현 on 2023/03/30.
+//
+
+import SwiftUI
+
+struct MenuView : View {
+    var body: some View {
+        Text("HI")
+    }
+}
