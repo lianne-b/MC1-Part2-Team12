9e6984f1340561c3d2c60142caf444cd78d9186c
금가경 <gageum0@gmail.com>
2023-03-30 21:27:54 +0900

    feat: 시작 화면 구현

--- CBLTesting.swiftpm/StartView.swift
+++ CBLTesting.swiftpm/StartView.swift
@@ -8,35 +8,31 @@
 import SwiftUI
 
 struct StartView: View {
+    @State private var isNextViewPresented = false
+    
     var body: some View {
-        NavigationView{
-            VStack(alignment: .center, spacing: 30 ) {
-                Text("우당탕탕 12조")
-                    .font(.system(size: 20, weight: .black, design: .rounded))
-                Text("내가 그린 기린 그림")
-                    .font(.system(size: 40, weight: .black, design: .rounded))
-                Text("니가 그린 그림일기")
-                    .font(.system(size: 40, weight: .black, design: .rounded))
-                    Button(action: {
-                        print("Button is Clicked")
-                    },
-                           label : {
-                        NavigationLink(destination: MenuView().navigationBarBackButtonHidden(true)){
-                            Text("시작하기")
-                                .padding(20)
-                                .foregroundColor(.black)
-                                .overlay(
-                                        RoundedRectangle(cornerRadius: 20)
-                                            .stroke(Color.black , lineWidth: 5))
-                                
-                        }
-                    }).padding(EdgeInsets(top: 100, leading: 0, bottom: 0, trailing: 0))
-                
-
+        ZStack {
+            NavigationView{
+                VStack(alignment: .center, spacing: 50 ) {
+                    Image("TitleA")
+                    Image("TitleB")
+                }
+                .background(
+                    Image("Background")
+                        .edgesIgnoringSafeArea(.all)
+                )
             }
-            .background(
-                Image("background"))
-            .edgesIgnoringSafeArea(.all)
+            if isNextViewPresented {
+                MenuView()
+                    .transition(.opacity)
+            }
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                withAnimation {
+                    isNextViewPresented = true
+                }
+            }
         }
     }
 }
