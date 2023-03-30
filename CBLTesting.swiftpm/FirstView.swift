55ef5955a728d9e14dae10b01dc84e01d55a3941
Toughie <airj907@gmail.com>
2023-03-31 03:12:05 +0900

    Feat: 시작 BGM 추가 및 다른 뷰 BGM 확인

--- CBLTesting.swiftpm/FirstView.swift
+++ CBLTesting.swiftpm/FirstView.swift
@@ -6,8 +6,10 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct FirstView: View {
+    @State var audioPlayer: AVAudioPlayer?
     var body: some View {
         NavigationView{
             VStack(alignment: .center, spacing: 30 ) {
@@ -17,28 +19,48 @@ struct FirstView: View {
                     .font(.system(size: 40, weight: .black, design: .rounded))
                 Text("니가 그린 그림일기")
                     .font(.system(size: 40, weight: .black, design: .rounded))
-                    Button(action: {
-                        print("Button is Clicked")
-                    },
-                           label : {
-                        NavigationLink(destination: ContentView().navigationBarBackButtonHidden(true)){
-                            Text("시작하기")
-                                .padding(20)
-                                .foregroundColor(.black)
-                                .overlay(
-                                        RoundedRectangle(cornerRadius: 20)
-                                            .stroke(Color.black , lineWidth: 5))
-                                
-                        }
-                    }).padding(EdgeInsets(top: 100, leading: 0, bottom: 0, trailing: 0))
+                Button(action: {
+                    print("Button is Clicked")
+                },
+                       label : {
+                    NavigationLink(destination: ContentView().navigationBarBackButtonHidden(true)){
+                        Text("시작하기")
+                            .padding(20)
+                            .foregroundColor(.black)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 20)
+                                    .stroke(Color.black , lineWidth: 5))
+                        
+                    }
+                }).padding(EdgeInsets(top: 100, leading: 0, bottom: 0, trailing: 0))
                 
-
+                
+            }        .onDisappear {
+                audioPlayer?.stop()
+                print("음악이 끊겨야됨")
             }
             .background(
                 Image("background"))
             .edgesIgnoringSafeArea(.all)
         }
+        
+        .onAppear {
+            guard let soundPath = Bundle.main.path(forResource: "firstMusic", ofType: "mp3") else { return }
+            let url = URL(fileURLWithPath: soundPath)
+            
+            do {
+                // Initialize audio player
+                audioPlayer = try AVAudioPlayer(contentsOf: url)
+                
+                // Play audio
+                audioPlayer?.play()
+                print("Music started playing.")
+            } catch {
+                print("Error playing music: \(error.localizedDescription)")
+            }
+        }
     }
+    
 }
 
 
