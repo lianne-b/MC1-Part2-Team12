7810dc36db42265f51c0e5783b96b75e68206b05
Ye Eun Choi <yeeunchoi@MDOM11401-1.postech.ac.kr>
2023-03-31 12:41:22 +0900

    [Feat] Add EndingView

---
+++ CBLTesting.swiftpm/WrapUpView.swift
@@ -0,0 +1,26 @@
+//
+//  WrapUpView.swift
+//  CBLTesting
+//
+//  Created by Ye Eun Choi on 2023/03/31.
+//
+
+import SwiftUI
+
+struct WrapUpView: View {
+    var body: some View {
+        VStack {
+            Image("WrapUpImage")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .ignoresSafeArea()
+                .navigationBarBackButtonHidden()
+        }
+    }
+}
+
+struct WrapUpView_Previews: PreviewProvider {
+    static var previews: some View {
+        WrapUpView()
+    }
+}
